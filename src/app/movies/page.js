d2c67d27fdd46704405dc1c3a72b9aaf42c6f9c6
amailemon98 <amailemon98@gmail.com>
2024-07-02 22:01:20 +0900

    react -> nextJS, framer-motion, AnimatePresence

---
+++ src/app/movies/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// localhost:3000/movies
+const Movies = () => {
+  return (
+    <div>Movies</div>
+  )
+}
+
+export default Movies
---
+++ src/app/page.js
@@ -0,0 +1,18 @@
+import Box from '@/components/presence/Box'
+import Slider from '@/components/slider/Slider'
+import React from 'react'
+
+// localhost:3000/
+// localhost:3000/movies
+// localhost:3000/coins
+
+const Home = () => {
+  return (
+    <div>
+      {/* <Slider /> */}
+      <Box />
+    </div>
+  )
+}
+
+export default Home
